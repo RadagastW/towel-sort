41bdf39b74e0b2a6d70386c6a88881a2e504b59f
RadagastW <radagastw@yandex.ru>
2021-03-12 14:25:32 +0600

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+module.exports = function towelSort(matrix) {
+    str = '';
+    result = [];
 
-// You should implement your task here.
+    if (matrix == undefined) return [];
 
-module.exports = function towelSort (matrix) {
-  return [];
+    matrix.forEach(function (item, index, array) {
+        (index % 2 == 0) ? str += ', ' + item.join(', '): str += ', ' + item.reverse().join(', ');
+    });
+
+    result = str.split(', ');
+    result = result.map(item => parseInt(item));
+    result.shift();
+
+    return result;
 }
